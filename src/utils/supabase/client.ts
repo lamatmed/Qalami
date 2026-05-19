9dc61825b8e296e19bdcf1c7e82621b6a87d2a36
Lamat Abdellahi <lamat032025@gmail.com>
2026-05-19 12:16:29 +0000

    suspend app

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -3,7 +3,7 @@ import { Database } from '@/types/supabase'
 
 export function createClient() {
     return createBrowserClient<Database>(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+        process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://placeholder-url.supabase.co',
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'placeholder-anon-key'
     )
 }
